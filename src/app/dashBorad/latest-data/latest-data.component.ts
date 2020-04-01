4557bab97eaa15ed54cadd6d46c65f80ad4038a1
Naeem Akram <na6574113@gmail.com>
2020-04-01 20:24:15 +0500

    add world map and guidelines componnent

--- src/app/dashBorad/latest-data/latest-data.component.ts
+++ src/app/dashBorad/latest-data/latest-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgZone } from "@angular/core";
+import { Component, OnInit, NgZone, OnDestroy } from "@angular/core";
 import { Papa } from "ngx-papaparse";
 import { AppServicesService } from "src/app/service/app-services.service";
 import { latestData } from "../../models/latestDataModel";
@@ -23,7 +23,7 @@ am4core.useTheme(am4themes_animated);
   templateUrl: "./latest-data.component.html",
   styleUrls: ["./latest-data.component.css"]
 })
-export class LatestDataComponent implements OnInit {
+export class LatestDataComponent implements OnInit, OnDestroy {
   // *************************************************************************************************************//
   //                                           Variable Declaration                                               *
   // *************************************************************************************************************//
@@ -56,7 +56,101 @@ export class LatestDataComponent implements OnInit {
     private zone: NgZone
   ) {}
 
-  ngAfterViewInit(): void {}
+  ngAfterViewInit(): void {
+    this.zone.runOutsideAngular(() => {
+      // Create map instance
+      let chart = am4core.create("chartdiv", am4maps.MapChart);
+      chart.projection = new am4maps.projections.Miller();
+
+      // Create map polygon series for world map
+      let worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
+      worldSeries.useGeodata = true;
+      worldSeries.geodata = am4geodata_worldLow;
+      worldSeries.exclude = ["AQ"];
+
+      let worldPolygon = worldSeries.mapPolygons.template;
+
+      worldPolygon.tooltipText =
+        "[bold font-size: 14px black]{name}[/]\n[black]Confirmed cases:[/] [bold font-size: 13px black]{value}[/]\n[black]Recovered Cases:[/] [bold font-size: 13px black]{Recovered}[/]\n[black]Total Deaths:[/] [bold font-size: 13px black]{Deaths}[/]";
+      worldPolygon.nonScalingStroke = true;
+
+      worldPolygon.strokeOpacity = 0.5;
+      worldPolygon.fill = am4core.color("#eee");
+      worldPolygon.propertyFields.fill = "color";
+
+      let hs = worldPolygon.states.create("hover");
+      hs.properties.fill = am4core.color("blue");
+
+      worldSeries.heatRules.push({
+        property: "fill",
+        target: worldSeries.mapPolygons.template,
+        min: am4core.color("#8AB4F8"),
+        max: am4core.color("#174EA6")
+      });
+
+      // Set up data for countries
+
+      let data = [];
+
+      for (var id in am4geodata_data_countries2) {
+        if (am4geodata_data_countries2.hasOwnProperty(id)) {
+          let country = am4geodata_data_countries2[id];
+
+          // this.AllCountry.filter(e => {
+          //   console.log(e.Country_Region == country.country);
+          // });
+
+          for (let index = 0; index < this.AllCountry.length; index++) {
+            const element = this.AllCountry[index];
+            if (
+              element.Country_Region === country.country ||
+              element.Country_Region === country.continent_code ||
+              element.Country_Region === id ||
+              element.code === id ||
+              element.code === country.continent_code
+            ) {
+              let obj = {
+                id: id,
+                // color: chart.colors.getIndex(
+                //   continents[country.continent_code]
+                // ),
+                map: country.maps[0],
+                country: country.country
+              };
+              obj["value"] = element.Confirmed;
+              obj["Recovered"] = element.Recovered;
+              obj["Deaths"] = element.Deaths;
+
+              data.push(obj);
+            }
+          }
+        }
+      }
+
+      worldSeries.data = data;
+
+      // Zoom control
+      chart.zoomControl = new am4maps.ZoomControl();
+
+      let homeButton = new am4core.Button();
+      homeButton.events.on("hit", function() {
+        worldSeries.show();
+        // countrySeries.show();
+        chart.goHome();
+      });
+
+      homeButton.icon = new am4core.Sprite();
+      homeButton.padding(7, 5, 7, 5);
+      homeButton.width = 30;
+      homeButton.icon.path =
+        "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
+      homeButton.marginBottom = 20;
+      homeButton.parent = chart.zoomControl;
+      homeButton.insertBefore(chart.zoomControl.plusButton);
+      this.spinner.hide();
+      this.chart = chart;
+    });
+  }
   ngOnDestroy() {
     this.zone.runOutsideAngular(() => {
       if (this.chart) {
@@ -70,14 +164,8 @@ export class LatestDataComponent implements OnInit {
   // *************************************************************************************************************//
 
   ngOnInit(): void {
+    this.spinner.show();
     this.allLocationData();
-    if (localStorage.getItem("LocationData")) {
-      this.mapFunction();
-    } else {
-      setTimeout(() => {
-        this.mapFunction();
-      }, 3000);
-    }
   }
 
   // *************************************************************************************************************//
@@ -400,99 +488,5 @@ export class LatestDataComponent implements OnInit {
   // *************************************************************************************************************//
   //                                                 World Map                                                     *
   // *************************************************************************************************************//
-  mapFunction() {
-    this.zone.runOutsideAngular(() => {
-      // Create map instance
-      let chart = am4core.create("chartdiv", am4maps.MapChart);
-      chart.projection = new am4maps.projections.Miller();
-
-      // Create map polygon series for world map
-      let worldSeries = chart.series.push(new am4maps.MapPolygonSeries());
-      worldSeries.useGeodata = true;
-      worldSeries.geodata = am4geodata_worldLow;
-      worldSeries.exclude = ["AQ"];
-
-      let worldPolygon = worldSeries.mapPolygons.template;
-
-      worldPolygon.tooltipText =
-        "[bold font-size: 14px black]{name}[/]\n[black]Confirmed cases:[/] [bold font-size: 13px black]{value}[/]\n[black]Recovered Cases:[/] [bold font-size: 13px black]{Recovered}[/]\n[black]Total Deaths:[/] [bold font-size: 13px black]{Deaths}[/]";
-      worldPolygon.nonScalingStroke = true;
-
-      worldPolygon.strokeOpacity = 0.5;
-      worldPolygon.fill = am4core.color("#eee");
-      worldPolygon.propertyFields.fill = "color";
-
-      let hs = worldPolygon.states.create("hover");
-      hs.properties.fill = am4core.color("blue");
-
-      worldSeries.heatRules.push({
-        property: "fill",
-        target: worldSeries.mapPolygons.template,
-        min: am4core.color("#8AB4F8"),
-        max: am4core.color("#174EA6")
-      });
-
-      // Set up data for countries
-
-      let data = [];
-
-      for (var id in am4geodata_data_countries2) {
-        if (am4geodata_data_countries2.hasOwnProperty(id)) {
-          let country = am4geodata_data_countries2[id];
-
-          // this.AllCountry.filter(e => {
-          //   console.log(e.Country_Region == country.country);
-          // });
-
-          for (let index = 0; index < this.AllCountry.length; index++) {
-            const element = this.AllCountry[index];
-            if (
-              element.Country_Region === country.country ||
-              element.Country_Region === country.continent_code ||
-              element.Country_Region === id ||
-              element.code === id ||
-              element.code === country.continent_code
-            ) {
-              let obj = {
-                id: id,
-                // color: chart.colors.getIndex(
-                //   continents[country.continent_code]
-                // ),
-                map: country.maps[0],
-                country: country.country
-              };
-              obj["value"] = element.Confirmed;
-              obj["Recovered"] = element.Recovered;
-              obj["Deaths"] = element.Deaths;
-
-              data.push(obj);
-            }
-          }
-        }
-      }
-
-      worldSeries.data = data;
-
-      // Zoom control
-      chart.zoomControl = new am4maps.ZoomControl();
-
-      let homeButton = new am4core.Button();
-      homeButton.events.on("hit", function() {
-        worldSeries.show();
-        // countrySeries.show();
-        chart.goHome();
-      });
-
-      homeButton.icon = new am4core.Sprite();
-      homeButton.padding(7, 5, 7, 5);
-      homeButton.width = 30;
-      homeButton.icon.path =
-        "M16,8 L14,8 L14,16 L10,16 L10,10 L6,10 L6,16 L2,16 L2,8 L0,8 L8,0 L16,8 Z M16,8";
-      homeButton.marginBottom = 20;
-      homeButton.parent = chart.zoomControl;
-      homeButton.insertBefore(chart.zoomControl.plusButton);
-      this.spinner.hide();
-      this.chart = chart;
-    });
-  }
+  mapFunction() {}
 }
