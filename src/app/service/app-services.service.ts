63d658ad3ab8d14476385e64d42b4c8f5886016f
Naeem Akram <na6574113@gmail.com>
2020-04-10 11:48:13 +0500

    update

--- src/app/service/app-services.service.ts
+++ src/app/service/app-services.service.ts
@@ -23,7 +23,7 @@ export class AppServicesService {
   // *************************************************************************************************************//
 
   AllLocation_URL: string =
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-07-2020.csv";
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-09-2020.csv";
 
   // AllLocation_URL: string =
   // "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/0" +
